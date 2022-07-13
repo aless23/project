e149c3aa7cb919c3cac6b5549f9b9bd28da5c2a4
Oleg <justifyaless23@gmail.com>
2022-07-13 23:08:32 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 "use strict";
 
-const category = "toys";
+var numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log(numberOfFilms);
 
-console.log('https://someurl.com/' + category);
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+var a = prompt('Один из последних просмотренных фильмов?', ''),
+	b = prompt('На сколько оцените его?', ''),
+	c = prompt('Один из последних просмотренных фильмов?', ''),
+	d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+// const category = "toys";
+
+// console.log('https://someurl.com/' + category);
 
 // console, alert, prompt, confirm
 
